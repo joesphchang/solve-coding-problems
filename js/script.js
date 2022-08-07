837cabcc6836206894854f5827c0be0d2237d97d
joesphchang <joesphschang@gmail.com>
2022-08-07 09:57:12 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+// Given 2 arrays, craete a function that let's a user know (true/false) whether these two arrays contain any commmon items 
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'i'];
+// should return false;
+
+// -------------------------------
+
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'x'];
+// should return true; 
+
+// function checkDoubles(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === sum) {
+//             sum.push(arr)
+//         }
+//     }
+// }
+
+// 2 parameters - arrays - no size limit 
+// return true or false
